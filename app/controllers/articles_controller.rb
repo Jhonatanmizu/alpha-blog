0f435802b61799f8aef54a8843133dbe9bd5aa58
jhonatanmizu <natanjesuss20@gmail.com>
2023-12-28 22:16:09 -0300

    :sparkles: feat: new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,23 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = Articles.all
+    @articles = Article.all
+  end
+  def new
   end
 
+  def create
+    @article = Article.new(params.require(:article).permit(:title,:description))
+    @article.save
+    redirect_to article_path(@article)
+  end
   def show
     # @ convert a variable into a instance variable
     @article = Article.find(params[:id])
   end
 
-
-
+  def destroy
+    @article = Article.destroy(params[:id])
+  end
 
 end
